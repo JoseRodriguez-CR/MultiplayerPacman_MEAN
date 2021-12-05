137af23e6127eede360e650f4bbe3987eb8a8bbf
JoseRodriguez-CR <josemariorodriguezc@gmail.com>
2021-12-04 22:57:15 -0600

    Uploading Files

---
+++ static/js/index.js
@@ -0,0 +1,55 @@
+console.log("Server On (Now)");
+
+let socket = io("http://localhost:8080");
+
+
+let username = prompt("Introduce your name:");
+//socket for message
+$('.formMessages').on('submit', function(event){
+    event.preventDefault();
+    let message = $('#message').val();
+    
+    let info={
+        name: username,
+        message: message,
+    };
+    socket.emit('information', info);
+});
+
+socket.on('msj', function(info){
+    let message = `<p>${info.name} :  ${info.message}</p>`;
+    $('.messages' ).append(message);
+});
+
+socket.on('display', function(info){
+    for(let i = 0; i<info.length;i++){
+        let message = `<p>${info[i].name} :  ${info[i].message}</p>`;
+        $('.messages' ).append(message);
+    }
+});
+
+//socket for username
+$('.formUsers').on('submit', function(event){
+    event.preventDefault();
+    let users = $('#formUsers').val();
+
+    let  user = {
+        name: username,
+        message: users,
+    };
+
+    //socket.emit('user', user);
+    socket.emit('user', user);
+});
+/*
+socket.on('msj', function(info){
+    let users = `<p>${info[i].name}</p>`;
+    $('.users' ).append(users);
+});*/
+
+socket.on('display', function(info){
+    for(let i = 0; i<info.length;i++){
+        let users = `<p>${info[i].message} </p>`;
+        $('.users' ).append(users);
+    }
+});
